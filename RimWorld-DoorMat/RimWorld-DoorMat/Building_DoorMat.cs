4596bedb7703a6320df1b37f2836f774c356059d
urty5656 <urty5656@gmail.com>
2016-07-19 12:02:49 +0900

    Initial upload

---
+++ RimWorld-DoorMat/RimWorld-DoorMat/Building_DoorMat.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Verse;
+
+namespace LT
+{
+    public class Building_DoorMat : Building
+    {
+        public override void Tick()
+        {
+            base.Tick();
+            if(!Gen.IsHashIntervalTick(this, 10))
+            {
+                return;
+            }
+            //Execute every 10 ticks
+            foreach(var current in this.OccupiedRect())
+            {
+                if(current.Impassable())
+                {
+                    continue;
+                }
+                var pawn = Find.Map.thingGrid.ThingAt<Pawn>(current);
+                if(pawn != null)
+                {
+                    pawn.filth.GetType().GetMethod("TryDropFilth", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(pawn.filth, null);
+                }
+            }
+        }
+    }
+}
